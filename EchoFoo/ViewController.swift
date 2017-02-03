5583983d991895733ee811e93689f7c1ee723590
sharonkass <kasssharon@gmail.com>
2017-02-03 06:08:53 -0500

    Initial Commit

---
+++ EchoFoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EchoFoo
+//
+//  Created by Sharon Kass on 2/3/17.
+//  Copyright © 2017 RoboTigers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
