1fe3d1e6ba5ba63964950f631a3b7c47c85356a9
sharonkass <kasssharon@gmail.com>
2017-02-03 06:46:36 -0500

    Add guts of the app. A core data model for Echo objects. A table view controller to view the list of echoes and to allow deletes of echoes. A view controller with an ECHO button to do the echo and save it to the data store.

---
+++ EchoFoo/EchoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  EchoViewController.swift
+//  EchoFoo
+//
+//  Created by Sharon Kass on 2/3/17.
+//  Copyright © 2017 RoboTigers. All rights reserved.
+//
+
+import UIKit
+
+class EchoViewController: UIViewController {
+
+    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    var dataForLabel : String = ""
+    
+    @IBAction func echo(_ sender: UIButton) {
+        print("Button pressed")
+        dataForLabel = text.text!
+        label.text = dataForLabel
+        
+        // write the value to the data store
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let echo = Echo(context: managedContext)
+        echo.text = dataForLabel
+        echo.created = NSDate()
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save the echo. \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        print("in viewDidLoad")
+        label.text = dataForLabel;
+    }
+
+}
